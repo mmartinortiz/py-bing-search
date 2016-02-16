fb0f41de5eb19e1d6dcca7e80c94f7e7f300037d
Manuel Martin-Ortiz <manuel@advicegames.com>
2016-02-16 16:02:18 +0100

    Updated to Python3

--- py_bing_search/py_bing_search.py
+++ py_bing_search/py_bing_search.py
@@ -1,10 +1,11 @@
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import requests
-import pdb
 import time
+
 
 class PyBingException(Exception):
     pass
+
 
 class PyBingSearch(object):
 
@@ -16,11 +17,20 @@ class PyBingSearch(object):
         self.safe = safe
 
     def search(self, query, limit=50, offset=0, format='json'):
-        ''' Returns the result list, and also the uri for next page (returned_list, next_uri) '''
+        """ Returns the result list, and also the uri for next page (returned_list, next_uri)
+        :param format: Output format. JSON by default
+        :param offset:
+        :param limit: Search results limit, 50 by default
+        :param query: String to be searched
+        """
         return self._search(query, limit, offset, format)
 
     def search_all(self, query, limit=50, format='json'):
-        ''' Returns a single list containing up to 'limit' Result objects'''
+        """ Returns a single list containing up to 'limit' Result objects
+        :param query: String to be searched
+        :param limit: Limit of the search, 50 by deafult
+        :param format: Output format, JSON by default
+        """
         results, next_link = self._search(query, limit, 0, format)
         while next_link and len(results) < limit:
             max = limit - len(results)
@@ -31,10 +41,10 @@ class PyBingSearch(object):
         return results
 
     def _search(self, query, limit, offset, format):
-        '''
+        """
         Returns a list of result objects, with the url for the next page bing search url.
-        '''
-        url = self.QUERY_URL.format(urllib2.quote("'{}'".format(query)), limit, offset, format)
+        """
+        url = self.QUERY_URL.format(urllib.parse.quote("'{}'".format(query)), limit, offset, format)
         r = requests.get(url, auth=("", self.api_key))
         try:
             json_results = r.json()
@@ -42,7 +52,8 @@ class PyBingSearch(object):
             if not self.safe:
                 raise PyBingException("Request returned with code %s, error msg: %s" % (r.status_code, r.text))
             else:
-                print "[ERROR] Request returned with code %s, error msg: %s. \nContinuing in 5 seconds." % (r.status_code, r.text)
+                print("[ERROR] Request returned with code {}, error msg: {}. \nContinuing in 5 seconds.".format(
+                        r.status_code, r.text))
                 time.sleep(5)
         try:
             next_link = json_results['d']['__next']
@@ -50,13 +61,14 @@ class PyBingSearch(object):
             if not self.safe:
                 raise PyBingException("Couldn't extract next_link: KeyError: %s" % kE)
             else:
-                print "Couldn't extract next_link: KeyError: %s" % kE
+                print("Couldn't extract next_link: KeyError: {}".foramt(kE))
                 time.sleep(3)
             next_link = ''
         return [Result(single_result_json) for single_result_json in json_results['d']['results']], next_link
 
+
 class Result(object):
-    '''
+    """
     The class represents a SINGLE search result.
     Each result will come with the following:
 
@@ -69,12 +81,12 @@ class Result(object):
     #Meta info#:
     meta.uri: the search uri for bing
     meta.type: for the most part WebResult
-    '''
+    """
 
     class _Meta(object):
-        '''
+        """
         Holds the meta info for the result.
-        '''
+        """
         def __init__(self, meta):
             self.type = meta['type']
             self.uri = meta['uri']
